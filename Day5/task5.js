65003eb421bde848dbd2f04da575091b2b0023af
Shubhshrivastava8085 <n112al241062@gmail.com>
2025-09-06 21:41:32 +0530

    Create task5.js

---
+++ Day5/task5.js
@@ -0,0 +1,23 @@
+[
+  {
+    "id": 1,
+    "name": "Gaurav Sharma",
+    "rollNumber": "AI088",
+    "course": "AIML",
+    "email": "gauravsharma@example.com"
+  },
+  {
+    "id": 2,
+    "name": "Sweta Shah",
+    "rollNumber": "AI069",
+    "course": "AIML",
+    "email": "swetashah@example.com"
+  },
+  {
+    "id": 3,
+    "name": "Rahul Singh",
+    "rollNumber": "AI303",
+    "course": "AIML",
+    "email": "rahulsingh@example.com"
+  }
+]
